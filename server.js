cd50faa01996b67947b3346a86383d82ecd3a0bb
ChicoMangusta <andreptorres@gmail.com>
2020-06-01 23:13:26 -0400

    building the server

--- server.js
+++ server.js
@@ -1,22 +1,33 @@
 const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 8080
-const path = require('path')
+const app     = express();
+const PORT    = process.env.PORT || 8080
+const path    = require('path')
 
 app.use(express.static('build'));
 
 
+// MongoDB Connection
+mongoose.connect(mongoURI, { useNewUrlParser: true }, () =>
+  console.log("MongoDB connection established:", mongoURI)
+);
 
 
+// Error / Disconnection
+db.on("error", (err) => console.log(err.message + " is Mongod not running?"));
+db.on("disconnected", () => console.log("mongo disconnected"));
 
 
-
+// Middleware
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 
 
 
 app.get('*', (req, res) => {
 	res.sendFile(path.join(`${__dirname}/build/index.html`))
 } )
+
+
 
 app.listen(PORT, () => {
 	console.log('listening on port', PORT)
