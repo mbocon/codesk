5414a12558d43c306de91be9155c1c45974596cc
Eric Arias <earias@Erics-MacBook-Pro.local>
2020-06-04 15:40:34 -0400

    cleaning up Register page

--- src/Register/index.js
+++ src/Register/index.js
@@ -4,29 +4,39 @@ import { Link } from "react-router-dom";
 import SignUpForm from "./signupform";
 import "./style.css";
 import logo from "../codeskLogo.png";
+import Footer from "../Footer"
 
 class Register extends Component {
   render() {
     return (
       // Navigation header on top of the page
       <div className="App">
-        <nav className="navbar navbar-light bg-light register-nav">
-          <Link to="/">
-            <img src={logo} alt="not found" />
-          </Link>
-          <div className="register-right">
-          <Link to="/" className="btn btn-outline-info">
-            Home page
-          </Link>
+          <nav className="navbar navbar-light bg-light register-nav">
+            <Link to="/">
+                <img src={logo} alt="not found" />
+                  </Link>
           
-          </div>
-        </nav>
-        <div className="register-container container container-fluid">
-          <SignUpForm />
-        </div>
-      </div>
+          {/* Link to Home page */}
+          <div className="register-right">
+              <Link to="/" className="btn btn-outline-info">
+                  Home page
+                    </Link>
+                      </div>
+                        </nav>
+
+
+        {/* Sign Up form  */}
+          <div className="register-container container container-fluid">
+            <SignUpForm />
+                </div>
+                    </div>
+
+
+
+        
     );
   }
 }
 
+        
 export default Register;
